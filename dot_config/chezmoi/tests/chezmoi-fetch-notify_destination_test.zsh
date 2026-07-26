7c1602644e1fbd42c092655851bfe7590fc7c431
Tatsuya Kamohara <tatsuya_kamohara@kyudentd.co.jp>
2026-07-26 18:11:38 +0900

    fix: 配布後テストを任意の作業ディレクトリで実行可能にする

--- dot_config/chezmoi/tests/chezmoi-fetch-notify_destination_test.zsh
+++ dot_config/chezmoi/tests/chezmoi-fetch-notify_destination_test.zsh
@@ -12,14 +12,17 @@ state_file="$test_tmp/chezmoi-state.boltdb"
 function test_Chezmoi配布後のFakeで更新通知テストが成功する() {
   mkdir -p "$destination_dir"
 
-  chezmoi \
-    --source "$source_dir" \
-    --destination "$destination_dir" \
-    --persistent-state "$state_file" \
-    apply --parent-dirs --source-path \
-    dot_config/chezmoi/tests/chezmoi-fetch-notify_test.zsh \
-    dot_config/chezmoi/tests/fakes/executable_osascript \
-    dot_local/bin/executable_chezmoi-fetch-notify
+  (
+    cd "$source_dir"
+    chezmoi \
+      --source "$source_dir" \
+      --destination "$destination_dir" \
+      --persistent-state "$state_file" \
+      apply --parent-dirs --source-path \
+      dot_config/chezmoi/tests/chezmoi-fetch-notify_test.zsh \
+      dot_config/chezmoi/tests/fakes/executable_osascript \
+      dot_local/bin/executable_chezmoi-fetch-notify
+  )
 
   CHEZMOI_FETCH_NOTIFY_SCRIPT="$destination_dir/.local/bin/chezmoi-fetch-notify" \
     zsh "$destination_dir/.config/chezmoi/tests/chezmoi-fetch-notify_test.zsh"
